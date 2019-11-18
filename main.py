117f0103ad1e444c08c017652373f309dc8a0a7e
BuildTools <mwhaley@csumb.edu>
2019-11-18 14:58:10 -0800

    API now takes user input to search

--- main.py
+++ main.py
@@ -4,13 +4,21 @@ from key import key
 
 # Misuxmatch API linked to track.search method
 trackSearch = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=we%20will%20rock%20you&quorum_factor=1" 
+lyricSearch = "&q_lyrics="
 
+userInput = input("Enter Lyrics: ")
+
+querystring = {
+	"q" : lyricSearch+userInput
+}
 # response = requests.get(trackSearch+apikey)
-response = requests.request("GET", trackSearch+key)
-print("Musixmatch lyrics API call: " + trackSearch+key + "\n")
+response = requests.request("GET", trackSearch+key+querystring['q'])
 
-print(response.status_code)
+print(response.url)
+
 # Gets rid of 'callback()' that was included in the api callback
 responseText = response.text[9:-2]
 json_data = json.loads(responseText)
 print(json_data)
+
+print(json_data["message"]["body"]["track_list"][0]["track"]["track_name"])
