cc31c35008050c30ca2076b327b06d8f81d28c47
BuildTools <mwhaley@csumb.edu>
2019-11-18 15:08:12 -0800

    changed qlyric to q

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from key import key
 
 # Misuxmatch API linked to track.search method
 trackSearch = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=we%20will%20rock%20you&quorum_factor=1" 
-lyricSearch = "&q_lyrics="
+lyricSearch = "&q="
 
 userInput = input("Enter Lyrics: ")
 
@@ -21,4 +21,4 @@ responseText = response.text[9:-2]
 json_data = json.loads(responseText)
 print(json_data)
 
-print(json_data["message"]["body"]["track_list"][0]["track"]["track_name"])
+#print(json_data["message"]["body"]["track_list"][0]["track"]["track_name"])
