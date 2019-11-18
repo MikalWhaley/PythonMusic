79e40a33c589733e4e413681952c3f75f0fea0e6
EmmanuelC1 <emcastillo@csumb.edu>
2019-11-18 15:14:27 -0800

    simplfyied API and added better endpoint

--- main.py
+++ main.py
@@ -3,22 +3,18 @@ import json
 from key import key
 
 # Misuxmatch API linked to track.search method
-trackSearch = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&q_lyrics=we%20will%20rock%20you&quorum_factor=1" 
-lyricSearch = "&q="
+trackSearch = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1"
 
-userInput = input("Enter Lyrics: ")
+# takes in inout from user and appends it querystring
+user_input = input("Enter Lyrics: ")
+querystring = "&q="+user_input
 
-querystring = {
-	"q" : lyricSearch+userInput
-}
+
 # response = requests.get(trackSearch+apikey)
-response = requests.request("GET", trackSearch+key+querystring['q'])
-
+response = requests.request("GET", trackSearch+key+querystring)
 print(response.url)
+print()
 
 # Gets rid of 'callback()' that was included in the api callback
 responseText = response.text[9:-2]
 json_data = json.loads(responseText)
-print(json_data)
-
-#print(json_data["message"]["body"]["track_list"][0]["track"]["track_name"])
