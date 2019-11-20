59c393c71901c840136c90a6e7aeef575e37dac1
EmmanuelC1 <emcastillo@csumb.edu>
2019-11-20 11:10:12 -0800

    filtered endpoint for better search results

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import requests
 import json
 from key import key
 
-# Misuxmatch API linked to track.search method
-trackSearch = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1"
+# Musixmatch API linked to track.search method
+trackSearch = "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1&s_track_rating=desc"
 
-# takes in inout from user and appends it querystring
+# takes in input from user and appends it to querystring
 user_input = input("Enter Lyrics: ")
 querystring = "&q="+user_input
 
 
 # response = requests.get(trackSearch+apikey)
 response = requests.request("GET", trackSearch+key+querystring)
-print(response.url)
-print()
 
-# Gets rid of 'callback()' that was included in the api callback
+# Gets rid of 'callback(' and ');' that was included in the api callback string
 responseText = response.text[9:-2]
+
+# loads the response text as json data
 json_data = json.loads(responseText)
+
