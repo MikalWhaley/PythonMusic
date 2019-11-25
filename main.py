caff2d3f88687e096da3cacef4a6a1f6b797408c
BuildTools <mwhaley@csumb.edu>
2019-11-25 15:03:42 -0800

    added code to grab info, still need genre

--- main.py
+++ main.py
@@ -19,3 +19,38 @@ responseText = response.text[9:-2]
 # loads the response text as json data
 json_data = json.loads(responseText)
 
+#print(json_data) # gets everything
+# print(json_data["message"]["body"]["track_list"][0]["track"]["track_name"])
+# print(json_data["message"]["body"]["track_list"][0]["track"]["album_name"])
+# print(json_data["message"]["body"]["track_list"][0]["track"]["artist_name"])
+# print(json_data["message"]["body"]["track_list"][0]["track"]["primary_genres"]["music_genre_list"][0]["music_genre"]["music_genre_name"])
+# print(json_data["message"]["body"]["track_list"][0]["track"]["primary_genres"]["music_genre_list"][1]["music_genre"]["music_genre_name"])
+# print("\n")
+
+print(response.url)
+
+song_names = []
+for i in range(7):
+	song_names.append(json_data["message"]["body"]["track_list"][i]["track"]["track_name"])
+
+
+album_names = []
+for i in range(7):
+	album_names.append(json_data["message"]["body"]["track_list"][i]["track"]["album_name"])
+
+artist_name = []
+for i in range(7):
+	artist_name.append(json_data["message"]["body"]["track_list"][i]["track"]["artist_name"])
+
+# genre_names = []
+# for i in range(7):
+# 	print("i",i)
+# 	for j in range(2):
+# 		print("j",j)
+# 		genre_names.append(json_data["message"]["body"]["track_list"][i]["track"]["primary_genres"]["music_genre_list"][j]["music_genre"]["music_genre_name"])
+
+
+# print(json_data["message"]["body"]["track_list"][1]["track"]["primary_genres"]["music_genre_list"][0]["music_genre"]["music_genre_name"])
+
+# if (json_data["message"]["body"]["track_list"][1]["track"]["primary_genres"]["music_genre_list"][0]["music_genre"]["music_genre_name"])!:
+# 	print("yep")
