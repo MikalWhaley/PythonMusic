800ad1f43335920b4eec235328cdb7639c9a4a89
EmmanuelC1 <emcastillo@csumb.edu>
2019-12-16 07:40:08 -0800

    backend finished :) just need some last minute css fromatting

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+song_names = []
+album_names = []
+artist_names = []
+track_id = []
+cover_art = []
+
 @app.route('/')
 def home():
 	return render_template('index.html')
@@ -29,37 +35,64 @@ def my_form_post():
 	# loads the response text as json data
 	json_data = json.loads(responseText)
 
-	song_names = []
-	album_names = []
-	artist_names = []
-	cover_art = []
+	global song_names
+	global album_names
+	global artist_names
+	global track_id
+	global cover_art
+
+	# Appends all songs, album, and artist names in separate lists
 	for i in range(len(json_data["message"]["body"]["track_list"])):
 		song_names.append(json_data["message"]["body"]["track_list"][i]["track"]["track_name"])
 		album_names.append(json_data["message"]["body"]["track_list"][i]["track"]["album_name"])
 		artist_names.append(json_data["message"]["body"]["track_list"][i]["track"]["artist_name"])
+		track_id.append(json_data["message"]["body"]["track_list"][i]["track"]["track_id"])
 
-
+	# Uses last.fm API to get album artwork
 	for i in range(len(album_names)):
 		art_Search = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=e88853d03b96eefc3b794d51cbe626ff&artist=" + artist_names[i] + "&album="  + album_names[i] + "&format=json"
 		cover_response = requests.request("GET", art_Search)
-		data = json.loads(cover_response.text)
+		cover_data = json.loads(cover_response.text)
 
-		if (len(data) == 3):
+		# If Album is not found
+		if (len(cover_data) == 3):
 			cover_art.append('/static/placeholder.png')
-			# cover_art.append(data["album"]["image"][3]["#text"])
-		elif(data["album"]["image"][3]["#text"] == ""):
+		# If Album is there but no artwork
+		elif(cover_data["album"]["image"][3]["#text"] == ""):
 			cover_art.append('/static/placeholder.png')
 		else:
-			cover_art.append(data["album"]["image"][3]["#text"])
-			# cover_art.append('/static/placeholder.png')
+			cover_art.append(cover_data["album"]["image"][3]["#text"])
+	
+	return render_template('results.html', song_names = song_names, album_names = album_names, artist_names = artist_names, cover_art = cover_art, track_id = track_id)
 
+@app.route('/lyrics/<track>')
+def lyrics(track):
+	global song_names
+	global album_names
+	global artist_names
+	global track_id
+	global cover_art
+	index = 0
+	album = ""
+	song = ""
+	artist = ""
+	cover = ""
+	
+	for id in track_id:
+		if(id == int(track)):
+			album = album_names[index]
+			song = song_names[index]
+			artist = artist_names[index]
+			cover = cover_art[index]
+		index += 1
 
+	lyricsSearch = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?format=jsonp&callback=callback&track_id=" + str(track) + key
+	lyrics_response = requests.request("GET", lyricsSearch)
+	lyrics_responseText = lyrics_response.text[9:-2]
+	lyrics_data = json.loads(lyrics_responseText)
 
-
-	# print(cover_art)
-	# return data
-	return render_template('results.html', song_names = song_names, album_names = album_names, artist_names = artist_names, cover_art = cover_art)
-
+	lyrics = lyrics_data['message']['body']['lyrics']['lyrics_body']
+	return render_template('lyrics.html', lyrics = lyrics, album = album, song = song, artist = artist, cover = cover)
 
 
 
